c7ef98eca65995de72eaef6c06d9c9be200bce17
Gabriela Vespoli <gabiat3@hotmail.com>
2021-02-28 16:02:28 -0300

    create routes, database, controllers and models

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,25 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
+import { router } from './routes';
+
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World /o/" });
-});
+app.use(express.json());
+app.use(router);
 
-app.post("/", (req, res) => {
-  return res.json({ message: "success!" });
-});
 
+// app.get("/", (req, res) => {
+//   return res.json({ message: "Hello World /o/" });
+// });
+
+// app.post("/", (req, res) => {
+//   return res.json({ message: "success!" });
+// });
 
 app.listen(3333, () => console.log("Server is running!")); 
 
 
 
-//npx tsc --init { cria o tsconfig.json no npm}
+//npx tsc --init { cria o tsconfig.json no npm }
+//npx typeorm migration:create -n CreateUsers { cria migrations com npm }
